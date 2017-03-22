0359c37d2fe51fd7aeb3f6399576b277bef6fd98
ramon <ramon.mecatec@gmail.com>
2017-03-22 16:55:39 -0300

    Environment scaled down a bit

---
+++ Assets/Assets/Scripts/Editor_Tools.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Editor_Tools : MonoBehaviour {
+
+	public int lvl_value = 10;
+	public GameObject Reference, Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Match_Objects( GameObject obj1,GameObject obj2)
+	{
+		// Only works with object with renderer
+		Vector3 sizeCalculated = obj1.GetComponent<Renderer> ().bounds.size;
+		//Vector3 sizeCalculated = Vector3.Scale( obj1.transform.localScale, obj1.GetComponent<Mesh>().bounds.size);
+		//obj2.transform.localScale = sizeCalculated;
+		obj2.transform.localScale = Vector3.Scale( obj1.transform.localScale, sizeCalculated);
+
+	}
+
+
+	public void Match_Objects( GameObject obj1,GameObject obj2, bool match_x = true, bool match_y = true ,bool match_z = true)
+	{
+		if( match_x )
+		{
+			this.Match_X ( obj1, obj2 );
+		}
+
+		if( match_y )
+		{
+			this.Match_Y ( obj1, obj2 );
+		}
+
+		if( match_z )
+		{
+			this.Match_Z ( obj1, obj2 );
+		}
+		
+	}
+
+	public void Match_X( GameObject obj1,GameObject obj2 )
+	{
+		Vector3 sizeCalculated = obj1.GetComponent<Renderer> ().bounds.size;
+		obj2.transform.localScale.Set( sizeCalculated.x, obj2.transform.localScale.y, obj2.transform.localScale.z );
+		
+	}
+
+	public void Match_Y( GameObject obj1,GameObject obj2 )
+	{
+		Vector3 sizeCalculated = obj1.GetComponent<Renderer> ().bounds.size;
+		obj2.transform.localScale.Set( obj2.transform.localScale.x, sizeCalculated.y,obj2.transform.localScale.z );
+	}
+
+	public void Match_Z( GameObject obj1,GameObject obj2 )
+	{
+		Vector3 sizeCalculated = obj1.GetComponent<Renderer> ().bounds.size;
+		//obj2.transform.localScale.z = sizeCalculated.z;
+		obj2.transform.localScale.Set( obj2.transform.localScale.x, obj2.transform.localScale.y, sizeCalculated.z );
+	}
+
+}
---
+++ Assets/Editor/GalerryEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Editor_Tools))]
+//public class GalerryEditor : MonoBehaviour {
+public class GalerryEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		// Draw default inspector
+		DrawDefaultInspector ();
+
+		Editor_Tools myEditor = (Editor_Tools)target;
+
+		// How to add an element and get it updated value
+		//myEditor.lvl_value = EditorGUILayout.IntField ("Experience", myEditor.lvl_value);
+
+		if( GUILayout.Button("Match_X") )
+		{
+			myEditor.Match_X ( myEditor.Reference, myEditor.Target );
+		}
+		else if( GUILayout.Button("Match_Y") )
+		{
+			myEditor.Match_Y ( myEditor.Reference, myEditor.Target );
+		}
+		else if( GUILayout.Button("Match_Z") )
+		{
+			myEditor.Match_Z ( myEditor.Reference, myEditor.Target );
+		}
+		else if( GUILayout.Button("Match") )
+		{
+			myEditor.Match_Objects ( myEditor.Reference, myEditor.Target );
+		}
+
+	}
+}
