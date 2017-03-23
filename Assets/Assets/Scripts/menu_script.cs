0f596eb29543651c8813c108544217d447fe4e27
ramon <ramon.mecatec@gmail.com>
2017-03-23 13:51:37 -0300

    Finished menu
    Added functions to menu's button. One to pass to next screen and the
    second to close the menu

---
+++ Assets/Assets/Scripts/menu_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_script : MonoBehaviour {
+
+	private int screen_index = 0;
+
+	public GameObject Panel1;
+	public GameObject Panel2;
+
+
+	public void update_screen()
+	{
+		if( screen_index == 0 )
+		{
+			Panel1.SetActive(false);
+			Panel2.SetActive(true);
+			screen_index++;
+		}
+		else if( screen_index == 1 )
+		{
+			Panel2.SetActive(false);
+		}
+	}
+
+}
